bc3b6d5f53b1c35a14787ea3c16badabd1bf6b3c
YukiEndo <yuki.a.endo@imjp.co.jp>
2020-01-31 16:20:07 +0900

    [add]redux-loggerの確認

--- src/05/js/store/index.js
+++ src/05/js/store/index.js
@@ -1,9 +1,14 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import moduleAReducer from '../modules/ModuleA';
 import thunk from 'redux-thunk';
+import { createLogger } from 'redux-logger';
+
+const logger = createLogger({
+  diff: true
+});
 
 const reducers = combineReducers({
   moduleA: moduleAReducer
 });
 
-export default createStore(reducers, applyMiddleware(thunk));
+export default createStore(reducers, applyMiddleware(thunk, logger));
